145e737c3738433d9f5ca39442fd3c6e0bdbbd6c
Szczepan <l.szczepan@wp.pl>
2020-05-22 12:00:56 +0200

    binding adapters for lists of details, not working for languages and currencies

--- app/src/main/java/com/example/worldatlas/utils/BindingAdapters.kt
+++ app/src/main/java/com/example/worldatlas/utils/BindingAdapters.kt
@@ -3,9 +3,11 @@ package com.example.worldatlas.utils
 import android.graphics.drawable.PictureDrawable
 import android.net.Uri
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
 import com.example.worldatlas.R
+import com.example.worldatlas.model.Currency
+import com.example.worldatlas.model.Language
 import com.example.worldatlas.utils.svg.GlideApp
 import com.example.worldatlas.utils.svg.SvgSoftwareLayerSetter
 
@@ -19,12 +21,26 @@ object BindingAdapters {
             GlideApp.with(this.context)
                 .`as`(PictureDrawable::class.java)
                 .error(R.drawable.ic_error_red_200dp)
-                .override(200,100)
+                .override(200, 100)
                 .centerInside()
                 .listener(SvgSoftwareLayerSetter())
 
         val uri = Uri.parse(imageUrl)
 
         requestBuilder.load(uri).into(this)
+    }
+
+    @JvmStatic
+    @BindingAdapter("listOfElements")
+    fun TextView.listOfElements(listOfElements: List<*>) {
+        val textToDisplay = StringBuilder()
+        listOfElements.forEach {
+            when (it) {
+                is String -> textToDisplay.append("$it\n")
+                is Currency -> textToDisplay.append("${it.name}\n")
+                is Language -> textToDisplay.append("${it.name}\n")
+            }
+        }
+        text = textToDisplay.toString()
     }
 }
